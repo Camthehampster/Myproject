ba940b6dd856403089c750b4ed1d8fc863fd651d
Code <Code@batc.local>
2018-08-01 12:04:57 -0600

    Jaren's commit

--- src/components/header.js
+++ src/components/header.js
@@ -1,14 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
+import paw from '../../images/paw.jpg'
+import bear from '../../images/bear.jpg'
 
-const Header = () => {
+
+class Header extends Component {
+
+    render() {
     return (
-        <div className="header">
+    <div>
+            <img className="imageone" width="150px" height="150px" src={ paw } />
+            <img className="image" width="150px" height="150px" src={ bear } />
+            <div className="header">
             <div className="header__skew"></div>
             <div className="header__border"></div>
-            <h1>Grizzee</h1>
-            <p>Learn about my youtube channel</p>
+            <h1 class="grizzee" >Grizzee</h1>
+            <p>Check my youtube channel out</p>
+
+
+
+  <h2 className="welcome">Welcome!</h2> 
+  
+
+
+
+
+        </div>
         </div>
     )
+}
 }
 
 export default Header;
