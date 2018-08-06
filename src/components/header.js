f32bba572c19d6d3cee1d9589fd24a52edd2111b
Code <Code@batc.local>
2018-08-06 12:00:44 -0600

    Grizzly bears and polar bears.

--- src/components/header.js
+++ src/components/header.js
@@ -9,16 +9,28 @@ class Header extends Component {
     return (
     <div>
             <img className="imageone" width="150px" height="150px" src={ paw } />
-            <img className="image" width="150px" height="150px" src={ bear } />
+            <center><img className="image" width="150px" height="150px" src={ bear } /></center>
             <div className="header">
             <div className="header__skew"></div>
             <div className="header__border"></div>
-            <h1 class="grizzee" >Grizzee</h1>
-            <p>Check my youtube channel out</p>
+            <h1 className="Grizzee" >Grizzee</h1>
+            <h2 className="textboxone" >Check my youtube channel out</h2>
+
+            
+  
+   
 
 
 
   <h2 className="welcome">Welcome!</h2> 
+
+
+   
+  <div>  
+  <div className="channellink"> 
+ My amazing youtube <a className="link" href="https://www.youtube.com/channel/UC3qp5QQQ2mWTlu4eLQrFP4w/featured?view_as=subscriber">channel</a>
+ </div>
+   </div> 
   
 
 
