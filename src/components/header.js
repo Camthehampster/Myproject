6e43dbe445172fa2bcd11d86f860b3a466c39310
Code <Code@batc.local>
2018-08-09 10:57:35 -0600

    final commit for CTS class.

--- src/components/header.js
+++ src/components/header.js
@@ -17,12 +17,11 @@ class Header extends Component {
             <h2 className="textboxone" >Check my youtube channel out</h2>
 
             
-  
-   
-
-
-
   <h2 className="welcome">Welcome!</h2> 
+
+
+ <button> <a className="Subbutton">Subscribe</a></button>
+ 
 
 
    
@@ -31,7 +30,35 @@ class Header extends Component {
  My amazing youtube <a className="link" href="https://www.youtube.com/channel/UC3qp5QQQ2mWTlu4eLQrFP4w/featured?view_as=subscriber">channel</a>
  </div>
    </div> 
+
+
+   <div className="Discordlink"> 
+ My amazing Discord <a className="Discord" href="https://discord.gg/waB6kC4">link</a>
+ </div>
   
+ <h1 className="Mybestvideo">My Best video's</h1>
+
+ <div className="videolink"> 
+ <a className="videolink" href="https://www.youtube.com/watch?v=k8cCEdb8HfM">link for video</a>
+ </div>
+
+
+ <div className="Video"> 
+ <iframe width="560" height="315" src="https://www.youtube.com/embed/k8cCEdb8HfM" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+ </div>
+
+
+<h1 className="aboutmygaming">About:</h1>
+
+
+<h2 className="Gamingtech">Gaming Tech</h2>
+
+<div className="Content">My content:
+I do minecraft videos.</div>
+
+<div className="Gaming"> 
+ My gaming <a className="GamingLink" href="https://www.amazon.com/PICTEK-Programmable-Breathing-Ergonomic-Computer/dp/B01FZ3BR5S/ref=sr_1_1_sspa?s=videogames&ie=UTF8&qid=1533744905&sr=1-1-spons&keywords=gaming+mouse&psc=1"> mouse</a>
+ </div>
 
 
 
